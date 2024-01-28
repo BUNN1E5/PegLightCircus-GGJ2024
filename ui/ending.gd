51a7b708bf0b6404f8e13e13b35f38154f5f7819
Miguel Campos <amodestduck@gmail.com>
2024-01-28 00:09:19 -0800

    change ending scene

--- ui/ending.gd
+++ ui/ending.gd
@@ -12,15 +12,11 @@ func _ready():
 func _process(delta):
 	pass
 
-
-
 func _on_play_again_button_pressed():
 	print("IMPLEMENT PLAY AGAIN BUTTON")
 
-
 func _on_image_button_pressed():
 	badEndingImage.visible = true
-
 
 func _on_bad_ending_image_pressed():
 	badEndingImage.visible = false
